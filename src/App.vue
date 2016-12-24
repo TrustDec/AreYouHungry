af2b263c8a6b01e3533ed71efc34205adaa52d01
TRUST\TrustTheBoy <603049583@qq.com>
2016-12-25 04:23:46 +0800

    适应手机1像素的边,新怎全局样式方法等

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
      <div class="tab-item">
       <a v-link="{path:'/goods'}">商品</a>      
      </div>
@@ -18,7 +18,6 @@
 
 <script>
 import header from './components/header/header.vue';
-
 export default{
   components: {
     'v-header': header
@@ -27,11 +26,14 @@ export default{
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      // border-bottom: 1px solid red
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
