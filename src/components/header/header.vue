ef6d72d56ae95c48090c464b5c091b898c04a9a2
TRUST\TrustTheBoy <603049583@qq.com>
2016-12-27 22:43:22 +0800

    add  this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -12,8 +12,8 @@
 				<div class="description">
 					{{seller.description}}/{{seller.deliveryTime}}分钟送达
 				</div>
-				<div v-if="seller.supports" class="supports">
-					<span class="icon"></span>
+				<div v-if="seller.supports" class="support">
+					<span class="icon" :class="classMap[seller.supports[0].type]"></span>
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
@@ -28,6 +28,9 @@
 			seller: {
 				type: Object
 			}
+		},
+		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 		}
 	};
 </script>
@@ -44,6 +47,8 @@
 			.avatar
 				display: inline-block
 				vertical-align: top
+				img
+					border-radius: 2px
 			.content
 				display: inline-block
 				margin-left: 16px
@@ -66,4 +71,25 @@
 					line-height: 12px
 					margin-bottom: 10px
 					font-size: 12px
+				.support
+					.icon
+						display: inline-block
+						width: 12px
+						height: 12px
+						margin-right: 4px
+						background-size: 12px 12px
+						background-repeat: no-repeat
+						&.decrease							
+							bg-image('decrease_1')
+						&.discount
+							bg-image('discount_1')
+						&.guarantee
+							bg-image('guarantee_1')
+						&.invoice
+							bg-image('invoice_1')
+						&.special
+							bg-image('special_1')
+						
+					
+					
 </style>
