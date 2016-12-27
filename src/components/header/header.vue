7dd95261b7b2e1ba230b10167ad9f8b2b4261c44
TRUST\TrustTheBoy <603049583@qq.com>
2016-12-27 22:13:51 +0800

    import img ->components  / header component ok

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,59 @@
 <template>
 	<div class="header">
-		header
+		<div class="content-wrapper">
+			<div class="avatar">
+				<img :src="seller.avatar" width="64">
+			</div>
+			<div class="content">
+				<div class="title">
+					<span class="brand"></span>
+					<span class="name">{{seller.name}}</span>
+				</div>
+				<div class="description">
+					{{seller.description}}/{{seller.deliveryTime}}分钟送达
+				</div>
+				<div v-if="seller.supports" class="supports">
+					<span class="icon"></span>
+					<span class="text">{{seller.supports[0].description}}</span>
+				</div>
+			</div>
+		</div>
+		<div class="bulletin-wrapper"></div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {};
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		}
+	};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+	 @import "../../common/stylus/mixin";
 	
+	.header
+		color: #fff
+		background-color: #000
+		.content-wrapper
+			padding: 24px 12px 18px 24px;
+			font-size: 0
+			.avatar
+				display: inline-block
+			.content
+				display: inline-block
+				margin-left: 16px
+				font-size: 16px
+				.title
+					margin: 2px 0 8px 0
+					.brand
+						width: 30px
+						height: 18px
+						display: inline-block
+						bg-image('brand')
+						background-size: 30px 18px
+						background-repeat: no-repeat
 </style>
