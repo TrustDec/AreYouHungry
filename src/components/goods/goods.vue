827bfeedb81ad3500e99653f9adf1a2a00f19ad3
TRUST\TrustTheBoy <603049583@qq.com>
2017-01-02 23:42:44 +0800

    没有遍历出数据。。。大爷的

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,20 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper"></div>
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="item in goods">
+					<span class="text">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+						{{item.name}}
+					</span>
+				</li>
+			</ul>
+		</div>
 		<div class="foods-wrapper"></div>		
 	</div>
 </template>
 
-<script type="text/ecmascript-6">
+<script type="text/ecmas cript-6">
 const ERR_OK = 0;
 	export default {
 		props: {
@@ -19,7 +28,9 @@ const ERR_OK = 0;
 			};
 		},
 		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 			this.$http.get('/api/goods').then((response) => {
+				console.log(response);
 				response = response.body;
 				if (response.errno === ERR_OK) {
 					this.goods = response.data;
