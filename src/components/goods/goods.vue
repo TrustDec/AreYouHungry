6ce85dd931d05ce9c6263978e29497cf70813a5e
TRUST\TrustTheBoy <603049583@qq.com>
2017-01-02 17:12:40 +0800

    content 布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,47 @@
 <template>
 	<div class="goods">
-		<!-- <div class="menu-wrapper"></div>
-				<div class="foods-wrapper"></div> -->	
-				goods	
+		<div class="menu-wrapper"></div>
+		<div class="foods-wrapper"></div>		
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {};
+const ERR_OK = 0;
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		},
+		data() {
+			return {
+				goods: []
+			};
+		},
+		created() {
+			this.$http.get('/api/goods').then((response) => {
+				response = response.body;
+				if (response.errno === ERR_OK) {
+					this.goods = response.data;
+					console.log(this.goods);
+				}
+			});
+		}
+	};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-	/*.goods
+	.goods
+		display: flex
+		position: absolute
+		width: 100%
+		top: 174px
+		bottom: 46px
+		overflow: hidden
 		.menu-wrapper
-		.foods-wrapper*/
+			flex: 0 0 80px
+			width: 80px
+			background: #f3f5f7
+		.foods-wrapper
+			flex: 1
 </style>
